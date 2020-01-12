bf3391d2724577fa06a0db74bd37d2c2bd2bff54
taketake25 <takeru31415@gmail.com>
2020-01-13 06:35:21 +0900

    add cookie

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -3,7 +3,7 @@ import autoBind from 'react-autobind';
 import './authentication.css';
 import { withRouter } from 'react-router';
 import PageHeader from './PageHeader';
-
+import { withCookies, Cookies } from 'react-cookie';
 
 class authentication extends Component {
     constructor(props) {
@@ -21,6 +21,14 @@ class authentication extends Component {
         this.handleSigninSubmit = this.handleSigninSubmit.bind(this);
     }
 
+    componentWillMount() { // コンポー年とがDOMに追加される前に一度だけ呼ばれる → 初期化処理に適している
+        const { cookies } = this.props;
+        this.state = {
+            user_name: cookies.get("user_name") || "",
+            user_password: cookies.get("user_password") || ""
+        }
+    }
+
     handleChangeUsername(event) {
         this.setState({ user_name: event.target.value });
     }
@@ -29,7 +37,40 @@ class authentication extends Component {
     }
     handleSubmit(e) {
         this.setState({ submit: "True" });
+        const { cookies } = this.props;
+        var user_name = this.state.user_name;
+        var user_password = this.state.user_password;
+
+        //ここにログイン情報が正しいかの確認処理を行うPOST
+
+        if (this.state.user_name == "" && this.state.user_password == "") {
+            this.setState({ alert: "情報が入力されていません" })
+        } else {
+            let user_info = {
+                user_name: this.state.user_name,
+                user_password: this.state.user_password,
+            };
+            fetch("http://192.168.0.13:4000/user/auth", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(user_info)
+            });
+            this.props.history.push('/')
+
+            // あるサイト曰く，このcookieの保存方法はセキュリティ上最悪の実装方法である．
+            // セキュリティの向上のためには，BASE64によるハッシュ値のみを保存し，1か月に1度の頻度で更新すること．
+            cookies.set('user_name', user_name, { path: '/' });
+            cookies.set('user_password', user_password, { path: '/' });
+
+            this.setState({ user_name });
+            this.setState({ user_password });
+
+            this.props.history.push('/')
+        }
     }
+
     handleSigninSubmit(e) {
         let new_user = {
             user_name: this.state.user_name,
@@ -55,7 +96,7 @@ class authentication extends Component {
                 <PageHeader />
                 <div className="auth">
                     <div className="signin">
-                        signin(mailがないからloginとの差別化が...)
+                        新規登録
                         <input
                             type="text"
                             placeholder="input your user_name"
@@ -71,7 +112,7 @@ class authentication extends Component {
                         <button onClick={this.handleSigninSubmit}>signinするで</button>
                     </div>
                     <div className="login">
-                        login
+                        ログイン
                         <input
                             type="text"
                             placeholder="input your user_name"
@@ -93,4 +134,4 @@ class authentication extends Component {
     }
 }
 
-export default withRouter(authentication);
+export default withCookies(authentication);
